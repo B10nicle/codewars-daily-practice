088865d254db267482b9e181ef1f9d036bd9c06d
Oleg Khilko <B10nicle@yandex.ru>
2022-10-06 09:54:32 +0300

    Codewars tasks : add OppositesAttract

---
+++ src/main/java/com/khilkoleg/functions/OppositesAttract.java
@@ -0,0 +1,13 @@
+package com.khilkoleg.functions;
+
+/**
+ * @author Oleg Khilko
+ */
+
+public class OppositesAttract {
+
+    public static boolean isLove(final int flower1, final int flower2) {
+
+    }
+
+}
