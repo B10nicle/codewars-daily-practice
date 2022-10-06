2fc259ec3c3668e2bf1d6afd52701a4db92b6f1c
Oleg Khilko <B10nicle@yandex.ru>
2022-10-06 09:48:54 +0300

    Codewars tasks : daily practice

---
+++ src/main/java/com/khilkoleg/functions/Main.java
@@ -0,0 +1,22 @@
+package com.khilkoleg.functions;
+
+import static com.khilkoleg.functions.Digitize.digitize;
+import static com.khilkoleg.functions.Accumulate.accum;
+import static com.khilkoleg.functions.NumberToString.numberToString;
+
+/**
+ * @author Oleg Khilko
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        digitize(0);
+        accum("Switch");
+        numberToString(5);
+
+
+    }
+
+}
