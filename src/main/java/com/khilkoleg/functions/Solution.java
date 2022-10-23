5a0a659c17e4bf07c08931a50beea31daa2f5440
Oleg Khilko <khilkoleg@gmail.com>
2022-10-23 22:06:43 +0300

    Codewars tasks : 2022-10-23

--- src/main/java/com/khilkoleg/functions/Solution.java
+++ src/main/java/com/khilkoleg/functions/Solution.java
@@ -22,4 +22,22 @@ public class Solution {
         return Arrays.asList(a).contains(x);
     }
 
+    public static String vaporCode(String s) {
+        var ca = s.toCharArray();
+        var sb = new StringBuilder();
+
+        for (char c : ca) {
+            if (c == ' ') {
+                continue;
+            } else {
+                sb.append(c).append(" ").append(" ");
+            }
+        }
+
+        return sb.toString().toUpperCase().trim();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Solution.vaporCode("Lets go to the movies"));
+    }
 }
